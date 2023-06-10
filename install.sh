0a3141be5fe4d69c64e17d235de2b2c25b5d1313
mahdi <mallakimahdi@gmail.com>
2023-06-10 11:09:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ for ((CLUSTER_INDEX=1;CLUSTER_INDEX<=${TOTAL_CLUSTERS};CLUSTER_INDEX++)); do
     
     for ((i=1;i<=${TOTAL_CLUSTERS};i++)); do
         CLUSTER_INDEX=$i envsubst < namespace.yaml > tmp/namespace-${i}.yaml
-        kubectl --context="cluster-${i}" apply -f tmp/namespace-${i}.yaml
+        kubectl --context="ctx-${i}" apply -f tmp/namespace-${i}.yaml
     done
 
     kubectl create secret generic cacerts -n istio-system \
@@ -20,24 +20,24 @@ for ((CLUSTER_INDEX=1;CLUSTER_INDEX<=${TOTAL_CLUSTERS};CLUSTER_INDEX++)); do
         --from-file=./gen-certs/certs/cluster-${CLUSTER_INDEX}/ca-key.pem \
         --from-file=./gen-certs/certs/root-cert.pem \
         --from-file=./gen-certs/certs/cluster-${CLUSTER_INDEX}/cert-chain.pem --dry-run -o yaml > tmp/certs.yaml
-    kubectl --context="cluster-${CLUSTER_INDEX}" -n istio-system apply -f tmp/certs.yaml
+    kubectl --context="ctx-${CLUSTER_INDEX}" -n istio-system apply -f tmp/certs.yaml
 
     envsubst < controlplane.yaml > tmp/controlplane-${CLUSTER_INDEX}.yaml
-    istioctl --context="cluster-${CLUSTER_INDEX}" install -y -f tmp/controlplane-${CLUSTER_INDEX}.yaml
+    istioctl --context="ctx-${CLUSTER_INDEX}" install -y -f tmp/controlplane-${CLUSTER_INDEX}.yaml
 
     envsubst < eastwest-gateway.yaml > tmp/eastwest-gateway-${CLUSTER_INDEX}.yaml
-    istioctl --context="cluster-${CLUSTER_INDEX}" install -y -f tmp/eastwest-gateway-${CLUSTER_INDEX}.yaml
+    istioctl --context="ctx-${CLUSTER_INDEX}" install -y -f tmp/eastwest-gateway-${CLUSTER_INDEX}.yaml
 
-    kubectl --context="cluster-${CLUSTER_INDEX}" apply -n istio-system -f ./expose-services.yaml
+    kubectl --context="ctx-${CLUSTER_INDEX}" apply -n istio-system -f ./expose-services.yaml
 
-    kubectl --context="cluster-${CLUSTER_INDEX}" get nodes -o json | jq '.items[].metadata.name' > tmp/nodes.txt
+    kubectl --context="ctx-${CLUSTER_INDEX}" get nodes -o json | jq '.items[].metadata.name' > tmp/nodes.txt
     NODES=`sed -z 's/\n/,/g;s/,$/\n/' tmp/nodes.txt`
-    kubectl --context="cluster-${CLUSTER_INDEX}" patch service istio-eastwestgateway --patch "{\"spec\": {\"externalIPs\": [${NODES}]}}" -n istio-system
+    kubectl --context="ctx-${CLUSTER_INDEX}" patch service istio-eastwestgateway --patch "{\"spec\": {\"externalIPs\": [${NODES}]}}" -n istio-system
 
     for ((i=1;i<=${TOTAL_CLUSTERS};i++)); do
         if [ ${i} != ${CLUSTER_INDEX} ]; then
-            istioctl --context="cluster-${i}" x create-remote-secret --name="cluster-${i}" > tmp/cluster-secret-${i}.yaml
-            kubectl --context="cluster-${CLUSTER_INDEX}" apply -f tmp/cluster-secret-${i}.yaml
+            istioctl --context="ctx-${i}" x create-remote-secret --name="cluster-${i}" > tmp/cluster-secret-${i}.yaml
+            kubectl --context="ctx-${CLUSTER_INDEX}" apply -f tmp/cluster-secret-${i}.yaml
         fi
     done
 
